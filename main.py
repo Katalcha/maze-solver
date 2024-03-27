8b4d2b098f60e697e5fe37705c48ec85a6dd84fb
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-03-27 19:45:22 +0100

    add Classes: Point, Line
    add Methods to draw on canvas

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+
+    # Testing Methods to draw stuff
+
+    line1 = Line(Point(50, 50), Point(400, 400))
+    line2 = Line(Point(75, 75), Point(300, 150))
+    line3 = Line(Point(150, 75), Point(144, 33))
+    diagonal = Line(Point(0,0), Point(800, 600))
+    
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "red")
+    win.draw_line(line3, "green")
+    win.draw_line(diagonal, "blue")
+
+    # Test end
+
     win.wait_for_close()
 
 main()
