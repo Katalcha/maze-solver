d24bb60179db7d66481969488ad8f489e1070c7e
R.Evers <83616447+Katalcha@users.noreply.github.com>
2024-04-07 15:46:24 +0200

    add more tests

--- test.py
+++ test.py
@@ -6,8 +6,27 @@ class Tests(unittest.TestCase):
         num_cols = 12
         num_rows = 10
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
-        self.assertEqual(len(m1._cells), num_cols)
-        self.assertEqual(len(m1._cells[0]), num_rows)
+        
+        len_cols = len(m1._cells)
+        len_rows = len(m1._cells[0])
+        
+        # expect len_cols == num_cols = True
+        # expect len_rows == num_rows = True
+        self.assertEqual(len_cols, num_cols)
+        self.assertEqual(len_rows, num_rows)
+    
+    def test_maze_create_cells_2(self):
+        num_cols = 200
+        num_rows = 18
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        
+        len_cols = len(m1._cells)
+        len_rows = len(m1._cells[0])
+
+        # expect len_cols == num_cols = True
+        # expect len_rows == num_rows = True
+        self.assertEqual(len_cols, num_cols)
+        self.assertEqual(len_rows, num_rows)
 
 if __name__ == "__main__":
     unittest.main()
